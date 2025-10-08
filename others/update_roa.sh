e93d795b62b1b31019e49387dad254d720408352
camera-2018 <40380042+camera-2018@users.noreply.github.com>
2025-10-08 15:42:22 +0800

    chore: update dn11 path

--- others/update_roa.sh
+++ others/update_roa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 FILE_URL="https://metadata.dn11.baimeow.cn/dn11_roa_bird2.conf"
-DEST_DIR="/etc/bird"
+DEST_DIR="/etc/bird/dn11"
 DEST_FILE="${DEST_DIR}/dn11_roa_bird2.conf"
 curl -sS -o "${DEST_FILE}" "${FILE_URL}" || {
     echo "Failed to download dn11_roa_bird2"
